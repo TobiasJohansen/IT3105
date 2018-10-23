f302fa613d3a7468cf42e4af58e79dc98182f08f
Tobias <tobjoh1994@gmail.com>
2018-10-23 03:17:09 +0200

    .

--- Module2/play_nim_vs_ai.py
+++ Module2/play_nim_vs_ai.py
@@ -5,14 +5,21 @@ from src.mcts import MCTS
 
 play = True
 while play:
+
+    # Create the brain of the MCTS AI(s), the list of Players, set up the Game and initialize the StateManager
     brain = MCTS(m=1000, rollout_batch_size=100)
     players = [Player("Tobias"), Player("AI-bert", brain=brain)]
     game = Game(players, starting_player_idx=0, total_nr_of_stones=15, max_selection=3)
     state_manager = StateManager(game)
+    
+    # Play until the game ends
     while not game.over:
+        # Update the internal state of the AI(s) brain
         brain.update(game.state)
+        # Request action from current player and execute it
         game.select_stones(game.current_player().take_turn(game, state_manager))
     
+    # Ask user if a new game should be started
     input_given = False
     while not input_given:
         inpt = input("Do you want to play another game of NIM? (y/n): ")
